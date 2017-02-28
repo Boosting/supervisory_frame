ec402093e68ac9d9cc56e409a882c5faf37c1865
dujiajun <dujiajun1994@163.com>
2017-02-27 23:24:29 -0800

    add text class name, id to targets  in the image

--- src/displayer.cpp
+++ src/displayer.cpp
@@ -18,17 +18,33 @@ void Displayer::stop(){
 }
 
 void Displayer::loop(){
-    int i=0;
+    map<Target::TARGET_CLASS, String> targetClassName;
+    targetClassName[Target::PERSON] = "person";
+    targetClassName[Target::BICYCLE] = "bicycle";
+    targetClassName[Target::BUS] = "bus";
+    targetClassName[Target::CAR] = "car";
+    targetClassName[Target::MOTORBIKE] = "motorbike";
+    targetClassName[Target::TRUCK] = "truck";
+
     while(!stopSignal) {
         Mat tmpImage = getImage();
         vector<Target> tmpTargets = getTargets();
         if(tmpImage.empty()) continue;
         for(Target &target: tmpTargets){
-            Rect region = target.getRegion();
-            rectangle(tmpImage, region, Scalar( 255, 0, 0 ), 1, 1);
+            Target::TARGET_CLASS targetClass = target.getClass();
+            auto it = targetClassName.find(targetClass);
+            if(it!=targetClassName.end()){
+                String name = it->second;
+                Rect region = target.getRegion();
+                String id(to_string(target.getId()));
+                int x = region.x, y = region.y;
+                rectangle(tmpImage, region, Scalar(255, 0, 0), 3, 1);
+                putText(tmpImage, name, Point(x+5, y+30), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 0), 1);
+                putText(tmpImage, id, Point(x+5, y+60), FONT_HERSHEY_SIMPLEX, 0.4, Scalar(0, 0, 255), 1);
+            }
         }
-        imwrite("/home/dujiajun/detect_result/" + to_string(i++) + ".jpg", tmpImage);
-        this_thread::sleep_for(chrono::milliseconds(10));
+        imshow("displayer", tmpImage);
+        waitKey(100);
     }
     runStatus = false;
     stopSignal = false;
