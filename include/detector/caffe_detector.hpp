7be5eda3b2d57535dd87ad34cad4b6db141d4300
dujiajun <dujiajun1994@163.com>
2017-03-07 22:35:35 -0800

    move detector functions

--- include/detector/caffe_detector.hpp
+++ include/detector/caffe_detector.hpp
@@ -16,9 +16,11 @@ class CaffeDetector: public MultiTargetDetector {
 public:
     CaffeDetector(const string& model_file, const string& trained_file, bool useGPU = true);
 protected:
+    boost::shared_ptr<Net<float> > net;
     Blob<float>* createImageBlob(const Mat& image);
     vector<vector<float> > getOutputData(string blob_name);
-    boost::shared_ptr<Net<float> > net;
+    vector<vector<Rect> > bbox_transform(const vector<vector<float> > &rois, const vector<vector<float> > &bbox_pred, const Mat& image);
+    vector<Target> nms(const vector<vector<Rect> > &bbox, const vector<vector<float> > &cls_prob, float thresh = 0.7, float min_trust_score = 0.1);
 };
 
 #endif //SUPERVISORY_FRAME_CAFFE_DETECTOR_HPP
--- include/multi_target_detector.hpp
+++ include/multi_target_detector.hpp
@@ -24,9 +24,6 @@ protected:
      * @brief A vector of target classes, the first element is background
      */
     vector<Target::TARGET_CLASS> idToClass;
-
-    vector<vector<Rect> > bbox_transform(const vector<vector<float> > &rois, const vector<vector<float> > &bbox_pred, const Mat& image);
-    vector<Target> nms(const vector<vector<Rect> > &bbox, const vector<vector<float> > &cls_prob, float thresh = 0.7, float min_trust_score = 0.1);
 };
 
 #endif //SUPERVISORY_FRAME_MULTI_TARGET_DETECTOR_HPP
